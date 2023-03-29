4bfb2ea3ecb3387b857ffcf0b375a922e36fb4a9
Sachin Tomar <s8120170044@gmail.com>
2023-03-29 16:52:32 +0530

    new file

---
+++ javaProgram2.java
@@ -0,0 +1,14 @@
+import java.time.chrono.ThaiBuddhistChronology;
+
+public class javaProgram2 {
+    int id,number;
+    String name;
+    public javaProgram2(int id,int number,String name){
+            this.id = id;
+            this.number = number;
+            this.name =  name;
+    }
+    public static void main(String rag[]){
+      
+    }    
+}
