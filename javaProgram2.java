7c892b838dc274bf55ad84f3edbc4f0712aad24f
Sachin Tomar <s8120170044@gmail.com>
2023-03-29 17:40:28 +0530

    again new

--- javaProgram2.java
+++ javaProgram2.java
@@ -8,11 +8,14 @@ public class javaProgram2 {
             this.number = number;
             this.name =  name;
     }
+    public void getDetails(){
+        System.out.println(id+"  "+name+"   "+number);
+    }
     public static void main(String rag[]){
         int id = 1;
         int number = 121;
         String name = "sachin";
       javaProgram2 obj = new javaProgram2(id,number,name);
-      
+        obj.getDetails();
     }    
 }
